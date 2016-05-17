4d41ce71d43b1a59878624ffe33257f09e6ac3ec
azulcontrole <caaraujo1420@gmail.com>
2016-05-17 10:58:40 -0300

    Meu Primeiro commit

---
+++ src/main/java/br/com/azulcontrole/App.java
@@ -0,0 +1,13 @@
+package br.com.azulcontrole;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
